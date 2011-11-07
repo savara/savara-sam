532b893e04f53688c7ac6dc44e17fdd01eb483cd
Jeff Yu <jeff.yuchang@gmail.com>
2011-11-07 22:56:40 +1100

    * temporary commit.

--- web/src/main/java/org/savara/sam/web/server/AQMonitorServiceImpl.java
+++ web/src/main/java/org/savara/sam/web/server/AQMonitorServiceImpl.java
@@ -3,15 +3,14 @@
  */
 package org.savara.sam.web.server;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.annotation.Resource;
+import javax.naming.InitialContext;
 
-import javax.inject.Inject;
-
+import org.infinispan.manager.CacheContainer;
 import org.savara.sam.activity.ActivitySummary;
-import org.savara.sam.aq.ActiveListener;
 import org.savara.sam.aq.ActiveQuery;
 import org.savara.sam.aq.ActiveQueryManager;
+import org.savara.sam.aq.server.ActiveQueryServer;
 import org.savara.sam.web.shared.AQMonitorService;
 import org.savara.sam.web.shared.dto.ResponseTime;
 import org.savara.sam.web.shared.dto.Statistic;
@@ -27,7 +26,10 @@ public class AQMonitorServiceImpl extends RemoteServiceServlet implements AQMoni
 	
 	private static final long serialVersionUID = 8965645007479773817L;
 	
-	@Inject
+	//TODO: this doesn't seem to work.
+	//@Resource(mappedName="java:jboss/infinispan/sam")
+	private CacheContainer _container;
+	
 	private ActiveQueryManager _activeQueryManager;
 	
 	private ActiveQuery<ActivitySummary> _startedTxns;
@@ -35,6 +37,13 @@ public class AQMonitorServiceImpl extends RemoteServiceServlet implements AQMoni
 	private ActiveQuery<ActivitySummary> _failedTxns;
 		
 	public AQMonitorServiceImpl() {
+		try {
+			InitialContext context = new InitialContext();
+			_container = (CacheContainer)context.lookup("java:jboss/infinispan/sam");
+			_activeQueryManager = new ActiveQueryServer();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 		_startedTxns = _activeQueryManager.getActiveQuery("PurchasingStarted");
 		_completedTxns = _activeQueryManager.getActiveQuery("PurchasingSuccessful");
 		_failedTxns = _activeQueryManager.getActiveQuery("PurchasingUnsuccessful");
@@ -44,18 +53,33 @@ public class AQMonitorServiceImpl extends RemoteServiceServlet implements AQMoni
 	public Statistic[] getStatistics() {
 
 		Statistic running = new Statistic();
-		running.setValue(_startedTxns.size() - _completedTxns.size() - _failedTxns.size());
+		if (_startedTxns != null && _completedTxns != null && _failedTxns != null) {
+			running.setValue(_startedTxns.size() - _completedTxns.size() - _failedTxns.size());
+		} else {
+			running.setValue(0);
+		}
 		running.setName("Running");
 		
 		Statistic successful = new Statistic();
-		successful.setValue(_completedTxns.size());
+		if (_completedTxns != null) {
+			successful.setValue(_completedTxns.size());
+		} else {
+			successful.setValue(0);
+		}
 		successful.setName("Successful");
 		
 		Statistic failed = new Statistic();
-		failed.setValue(_failedTxns.size());
+		if (_failedTxns != null) {
+			failed.setValue(_failedTxns.size());
+		} else {
+			failed.setValue(0);
+		}
 		failed.setName("Unsuccessful");
 		
 		 Statistic[] result = new Statistic[]{running, successful, failed};
+		 if (_startedTxns != null) {
+			 System.out.println("===> " + _startedTxns.size());
+		 }
 		 return result;
 	}
 
