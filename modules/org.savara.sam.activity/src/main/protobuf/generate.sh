8a4d98805a70c818376260267307ca8d0b858394
objectiser <gary@pi4tech.com>
2012-01-09 11:33:55 +0000

    Re-initialize the SAM repo

--- modules/org.savara.sam.activity/src/main/protobuf/generate.sh
+++
@@ -1,8 +0,0 @@
-#sh
-
-# This command generates the Activity.proto Java classes.
-# NOTE: It must be run from the folder containing the script, and the protobuf compiler
-# must have already been installed on your system
-
-protoc --java_out=../java ServiceModel.proto
-protoc --java_out=../java ActivityModel.proto
